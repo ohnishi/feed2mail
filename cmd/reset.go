4112354eeaac9ec0f53d8a21a29830a41ffd4563
ohnishi <48851954+ohnishi@users.noreply.github.com>
2022-11-21 18:45:29 +0900

    add

--- cmd/reset.go
+++ cmd/reset.go
@@ -8,7 +8,7 @@ import (
 var subscriptions = []subscription{
 	{Title: "まめきちまめこニートの日常", URL: "https://mamekichimameko.blog.jp/index.rdf", Fetched: time.Time{}},
 	{Title: "くるねこ大和", URL: "https://blog.goo.ne.jp/kuru0214/rss2.xml", Fetched: time.Time{}},
-	{Title: "梅屋敷商店街のランダム・ウォーカー", URL: "http://randomwalker.blog19.fc2.com/?xml", Fetched: time.Time{}},
+	{Title: "梅屋敷商店街のランダム・ウォーカー", URL: "https://randomwalker.blog.fc2.com/?xml", Fetched: time.Time{}},
 	{Title: "はてなブックマーク - 人気エントリー - テクノロジー", URL: "https://b.hatena.ne.jp/hotentry/it.rss", Fetched: time.Time{}},
 	{Title: "Publickey", URL: "https://www.publickey1.jp/atom.xml", Fetched: time.Time{}},
 	{Title: "Zennのトレンド", URL: "https://zenn.dev/feed", Fetched: time.Time{}},
