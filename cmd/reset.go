84c3b6d2cef9189c8070fb4ebdfcc8838ee28e71
ohnishi <48851954+ohnishi@users.noreply.github.com>
2023-04-22 03:47:58 +0900

    add

--- cmd/reset.go
+++ cmd/reset.go
@@ -9,13 +9,9 @@ var subscriptions = []subscription{
 	{Title: "まめきちまめこニートの日常", URL: "https://mamekichimameko.blog.jp/index.rdf", Fetched: time.Time{}},
 	{Title: "くるねこ大和", URL: "https://blog.goo.ne.jp/kuru0214/rss2.xml", Fetched: time.Time{}},
 	{Title: "梅屋敷商店街のランダム・ウォーカー", URL: "https://randomwalker.blog.fc2.com/?xml", Fetched: time.Time{}},
-	{Title: "Yahoo!ニュース・トピックス - IT", URL: "https://news.yahoo.co.jp/rss/topics/it.xml", Fetched: time.Time{}},
-	{Title: "Yahoo!ニュース・トピックス - 経済", URL: "https://news.yahoo.co.jp/rss/topics/business.xml", Fetched: time.Time{}},
 	{Title: "Publickey", URL: "https://www.publickey1.jp/atom.xml", Fetched: time.Time{}},
-	{Title: "Zennのトレンド", URL: "https://zenn.dev/feed", Fetched: time.Time{}},
 	{Title: "Rebuild", URL: "https://feeds.rebuild.fm/rebuildfm", Fetched: time.Time{}},
-	{Title: "ギズモード・ジャパン", URL: "https://www.gizmodo.jp/index.xml", Fetched: time.Time{}},
-	{Title: "はてなブックマーク", URL: "https://b.hatena.ne.jp/hotentry/it.rss", Fetched: time.Time{}},
+	{Title: "入江 慎吾", URL: "https://iritec.jp/feed/", Fetched: time.Time{}},
 }
 
 func resetSubscriptions(dest string) error {
