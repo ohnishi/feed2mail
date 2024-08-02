01a537e9201168cb3362ceb0f9c649263e5ab8e0
ohnishi <48851954+ohnishi@users.noreply.github.com>
2024-08-02 14:11:32 +0900

    add

--- cmd/reset.go
+++ cmd/reset.go
@@ -9,20 +9,15 @@ import (
 var subscriptions = []subscription{
 	{Title: "まめきちまめこニートの日常", URL: "https://mamekichimameko.blog.jp/index.rdf", Fetched: time.Time{}},
 	{Title: "くるねこ大和", URL: "https://blog.goo.ne.jp/kuru0214/rss2.xml", Fetched: time.Time{}},
-	{Title: "梅屋敷商店街のランダム・ウォーカー", URL: "https://randomwalker.blog.fc2.com/?xml", Fetched: time.Time{}},
+	{Title: "価格.comマガジン", URL: "https://kakakumag.com/rss/", Fetched: time.Time{}},
 	{Title: "Publickey", URL: "https://www.publickey1.jp/atom.xml", Fetched: time.Time{}},
 	{Title: "Zennのトレンド", URL: "https://zenn.dev/feed", Fetched: time.Time{}},
-	{Title: "CiLEL", URL: "https://cilel.jp/feed/", Fetched: time.Time{}},
 	{Title: "ログミーTech", URL: "https://logmi.jp/feed/public-tech.xml", Fetched: time.Time{}},
-	{Title: "ガジェラン", URL: "https://gadgelaun.com/?feed=rss2", Fetched: time.Time{}},
-	{Title: "IIJ Engineers Blog", URL: "https://eng-blog.iij.ad.jp/feed", Fetched: time.Time{}},
-	{Title: "Money Forward Developers Blog", URL: "https://moneyforward-dev.jp/feed", Fetched: time.Time{}},
 	{Title: "はてなブックマーク", URL: "https://b.hatena.ne.jp/hotentry/it.rss", Fetched: time.Time{}},
 	{Title: "ITmedia", URL: "https://rss.itmedia.co.jp/rss/2.0/topstory.xml", Fetched: time.Time{}},
-	{Title: "価格.comマガジン", URL: "https://kakakumag.com/rss/", Fetched: time.Time{}},
-	{Title: "鈴木正行", URL: "https://note.com/redturtle0721/rss", Fetched: time.Time{}},
-	{Title: "NHKニュース|社会", URL: "https://www3.nhk.or.jp/rss/news/cat1.xml", Fetched: time.Time{}},
+	{Title: "connpass 新着イベント", URL: "https://connpass.com/explore/ja.atom", Fetched: time.Time{}},
 	{Title: "NHKニュース|経済", URL: "https://www3.nhk.or.jp/rss/news/cat5.xml", Fetched: time.Time{}},
+	{Title: "梅屋敷商店街のランダム・ウォーカー", URL: "https://randomwalker.blog.fc2.com/?xml", Fetched: time.Time{}},
 }
 
 func resetSubscriptions(dest string) error {
