969061289ec827ebdc3d365c6779f63008f3a419
ohnishi <48851954+ohnishi@users.noreply.github.com>
2024-12-07 13:10:30 +0900

    add

--- cmd/reset.go
+++ cmd/reset.go
@@ -9,14 +9,13 @@ import (
 var subscriptions = []subscription{
 	{Title: "まめきちまめこニートの日常", URL: "https://mamekichimameko.blog.jp/index.rdf", Fetched: time.Time{}},
 	{Title: "くるねこ大和", URL: "https://blog.goo.ne.jp/kuru0214/rss2.xml", Fetched: time.Time{}},
-	{Title: "Publickey", URL: "https://www.publickey1.jp/atom.xml", Fetched: time.Time{}},
-	{Title: "Zennのトレンド", URL: "https://zenn.dev/feed", Fetched: time.Time{}},
-	{Title: "ログミーBusiness", URL: "https://logmi.jp/RSS/feed.xml", Fetched: time.Time{}},
 	{Title: "はてなブックマーク", URL: "https://b.hatena.ne.jp/hotentry/it.rss", Fetched: time.Time{}},
 	{Title: "ITmedia", URL: "https://rss.itmedia.co.jp/rss/2.0/topstory.xml", Fetched: time.Time{}},
+	{Title: "CoinPost", URL: "https://coinpost.jp/?feed=rss2", Fetched: time.Time{}},
 	{Title: "NHKニュース|経済", URL: "https://www3.nhk.or.jp/rss/news/cat5.xml", Fetched: time.Time{}},
+	{Title: "Zennのトレンド", URL: "https://zenn.dev/feed", Fetched: time.Time{}},
+	{Title: "Publickey", URL: "https://www.publickey1.jp/atom.xml", Fetched: time.Time{}},
 	{Title: "梅屋敷商店街のランダム・ウォーカー", URL: "https://randomwalker.blog.fc2.com/?xml", Fetched: time.Time{}},
-	{Title: "エムスリーテックブログ", URL: "https://www.m3tech.blog/rss", Fetched: time.Time{}},
 }
 
 func resetSubscriptions(dest string) error {
