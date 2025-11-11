1ad9b17a566a2b0ff13079018a66823c121bcda5
ohnishi <48851954+ohnishi@users.noreply.github.com>
2025-11-12 00:11:25 +0900

    add

--- main.go
+++ main.go
@@ -19,6 +19,7 @@ var subscriptions = []models.Subscription{
 	{Title: "Publickey", URL: "https://www.publickey1.jp/atom.xml", Fetched: time.Time{}},
 	{Title: "梅屋敷商店街のランダム・ウォーカー", URL: "https://randomwalker.blog.fc2.com/?xml", Fetched: time.Time{}},
 	{Title: "たつをの ChangeLog", URL: "https://chalow.net/cl.rdf", Fetched: time.Time{}},
+	{Title: "市況かぶ全力２階建", URL: "https://kabumatome.doorblog.jp/atom.xml", Fetched: time.Time{}},
 
 	{Title: "まめきちまめこニートの日常", URL: "https://mamekichimameko.blog.jp/index.rdf", Fetched: time.Time{}},
 	{Title: "チェンソーマン", URL: "https://shonenjumpplus.com/rss/series/3270375685341574005", Fetched: time.Time{}},
