79028f1acbc51e6b87077d0a97581c43f01f8c93
ohnishi <48851954+ohnishi@users.noreply.github.com>
2026-05-23 06:25:01 +0900

    add

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ var subscriptions = []models.Subscription{
 	{Title: "ITmedia", URL: "https://rss.itmedia.co.jp/rss/2.0/topstory.xml", Fetched: time.Time{}},
 	{Title: "Zenn", URL: "https://zenn.dev/feed", Fetched: time.Time{}},
 	{Title: "Publickey", URL: "https://www.publickey1.jp/atom.xml", Fetched: time.Time{}},
+	{Title: "窓の杜", URL: "https://forest.watch.impress.co.jp/data/rss/1.0/wf/feed.rdf", Fetched: time.Time{}},
 	// {Title: "市況かぶ全力２階建", URL: "https://kabumatome.doorblog.jp/atom.xml", Fetched: time.Time{}},
 	// {Title: "勝間和代が徹底的にマニアックな話をアップするブログ", URL: "https://katsumakazuyo.hatenablog.com/rss", Fetched: time.Time{}},
 	// {Title: "The GitHub Blog", URL: "https://github.blog/feed/", Fetched: time.Time{}},
